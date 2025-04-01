7caebd748f50cd9a34bda4d33a6883187c20f679
thibsignbib <50444352+thibsignbib@users.noreply.github.com>
2025-04-01 16:09:45 +0000

    Adding trisomie 21 logo

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,17 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.overlay}>
-        <h1 className={styles.title}>Hello World</h1>
+        <h1 className={styles.title}>Chronodome21</h1>
         <div className={styles.socials}>
           <a href="https://twitter.com" className={styles.button}>Twitter</a>
           <a href="https://linkedin.com" className={styles.button}>LinkedIn</a>
           <a href="https://github.com" className={styles.button}>GitHub</a>
         </div>
+        <img
+          src="/images/logo_trisomie_21_pdd.png"
+          alt="Mon logo"
+          className={styles.logo}
+        />
       </div>
     </div>
   );
