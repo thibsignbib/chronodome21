79dd307f5e34ad52d2c2ad9be702f70a9cbc49d6
thibsignbib <50444352+thibsignbib@users.noreply.github.com>
2025-04-01 20:45:08 +0000

    finalizing text

--- app/page.tsx
+++ app/page.tsx
@@ -48,21 +48,51 @@ export default function Home() {
         </div>
       </div>
 
+      <div className={styles.contentRow}>
+        <div className={styles.textBlock}>
+            <h1>Prix et inscriptions</h1>
+            <br />
+            <p>
+              Vous pouvez récupérer votre dossard <b>gratuitement</b> en vous présentant simplement au départ, le jour de l'événement. Nous vous proposons de contribuer via un don libre à l'organisation, dont l'intégralité des bénéfices reviendra à l'association Trisomie 21 Puy de Dôme.
+            </p>
+        </div>
+        <div> className={styles.textBlock}>
+          <a href="https://pots.lydia.me/collect/pots?id=71624-chronodome-2-1&fbclid=IwZXh0bgNhZW0CMTAAAR0s0ZyZlPr_sNZA7milhHObpNjSjEUxTZ2KIT3oL" className={styles.button} target="_blank" rel="noopener noreferrer">
+            <FaEuroSign /> Faire un don
+          </a>
+        </div>
+      </div>
+
       <div className={styles.contentRowDoubleText}>
-      <div className={styles.textBlock}>
-          <h1>Emplacement du parking et du départ de la boucle</h1>
-          <p>
-          Quand vous venez de Clermont depuis le col de ceyssat, continuez en direction de Ceyssat et jusqu'à une entrée de chemin signalée par des ballons et rubalises. Tournez sur votre droite, puis suivez la rubalise jusqu'au monument "des fusillés ardents".
-          <br /><br />
-          <a href="https://www.google.com/maps/place/45%C2%B046'26.9%22N+2%C2%B056'11.5%22E/@45.7702757,2.9333126,14.72z/data=!4m4!3m3!8m2!3d45.774131!4d2.93653?entry=ttu&g_ep=EgoyMDI1MDMzMC4wIKXMDSoASAFQAw%3D%3D">Lien Google Maps</a>
-          </p>
+        <div className={styles.textBlock}>
+            <h1>La date</h1>
+            <br />
+            <p>
+              La Chronodôme 2.1 aura lieu le samedi 7 juin à partir de 19h et se terminera donc le dimanche 8 juin à 16h.
+            </p>
         </div>
         <div className={styles.textBlock}>
-          <h1>La date</h1>
-          <br />
-          <p>
-            La Chronodôme 2.1 aura lieu le samedi 7 juin à partir de 19h et se terminera donc le dimanche 8 juin à 16h.
-          </p>
+            <h1>Emplacement du parking et du départ</h1>
+            <p>
+            Quand vous venez de Clermont depuis le col de ceyssat, continuez en direction de Ceyssat et jusqu'à une entrée de chemin signalée par des ballons et rubalises. Tournez sur votre droite, puis suivez la rubalise jusqu'au monument "des fusillés ardents".
+            <br /><br />
+            <a href="https://www.google.com/maps/place/45%C2%B046'26.9%22N+2%C2%B056'11.5%22E/@45.7702757,2.9333126,14.72z/data=!4m4!3m3!8m2!3d45.774131!4d2.93653?entry=ttu&g_ep=EgoyMDI1MDMzMC4wIKXMDSoASAFQAw%3D%3D">Lien Google Maps</a>
+            </p>
+        </div>
+      </div>
+
+      <div className={styles.contentRow}>
+        <div className={styles.textBlock}>
+            <h1>Restauration</h1>
+            <br />
+            <p>
+              Un stand de ravitaillement sera présent au départ de la boucle, avec plein de délices sucrés et salés. Une paella sera également proposée de 12h à 17h le dimanche 8 juin, au prix de 10€ la (grosse) barquette.
+            </p>
+        </div>
+        <div> className={styles.textBlock}>
+        <a href="https://doodle.com/sign-up-sheet/participate/2e77d6a5-d2b8-49dc-94ab-eea3224209fd/select" className={styles.button} target="_blank" rel="noopener noreferrer">
+            <FaPlus /> L'argument nourriture a fini de me convaincre, je m'inscris !!
+          </a>
         </div>
       </div>
     </section>
