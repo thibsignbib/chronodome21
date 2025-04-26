c5cd8428d7a47787f5de8bc9eadc60bbd91a10df
thibsignbib <50444352+thibsignbib@users.noreply.github.com>
2025-04-26 22:30:43 +0000

    adding news page

---
+++ app/news/page.tsx
@@ -0,0 +1,9 @@
+export default function NewsPage() {
+    return (
+      <div className="p-4">
+        <h1 className="text-3xl font-bold mb-4">Les News 📰</h1>
+        <p>Bienvenue dans l'espace News !</p>
+      </div>
+    );
+  }
+  
