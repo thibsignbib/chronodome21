9f8376adca7fd0f2c424bf5a2d32edd602a6266b
thibsignbib <50444352+thibsignbib@users.noreply.github.com>
2025-04-27 20:52:41 +0000

    bugfix

--- components/CreateArticleForm.tsx
+++ components/CreateArticleForm.tsx
@@ -64,7 +64,7 @@ export default function CreateArticleForm() {
         {
           type,
           title,
-          text,
+          content,
           images: [], // on ajoutera l'upload plus tard
         },
       ])
